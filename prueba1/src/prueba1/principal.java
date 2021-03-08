5c4c387dec4f0c89a2500d32300dc080b3651cb3
error <error@192.168.10.10>
2021-03-08 20:38:59 +0100

    Hago un cambio

--- prueba1/src/prueba1/principal.java
+++ prueba1/src/prueba1/principal.java
@@ -5,6 +5,8 @@ public class principal {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo java-git");
+		
+		System.out.println("Introduzco un segundo cambio");
 
 	}//fin main
 
